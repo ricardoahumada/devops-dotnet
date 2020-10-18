dccf34b967abaa1eb816d8f09b117405147ea280
ricardonetmind <ricardoag@expert.netmind.es>
2020-10-18 07:18:28 +0000

    cpp multi project

---
+++ projects/gradle/gradle-multi-project-cpp/Readme.md
@@ -0,0 +1,2 @@
+# to Upload
+- Execute: gradle publish
---
+++ projects/gradle/gradle-multi-project-cpp/cpp-calculator/src/main/cpp/cpp-calculator.cpp
@@ -0,0 +1,10 @@
+#include "cpp-calculator.h"
+#include <iostream>
+
+void UsingCalculator() {
+    std::cout << "Using C++ calculator ..." << std::endl;
+}
+
+int add(int a, int b){
+    return a+b;
+}
